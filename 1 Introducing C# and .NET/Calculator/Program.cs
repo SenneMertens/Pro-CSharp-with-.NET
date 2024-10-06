a80e062ec73a00d3e47501b243e3fc5ddc17eb44
Senne Mertens <Senne.Mertens@Outlook.com>
2024-10-06 15:55:02 +0200

    Introducing C# and .NET Update

---
+++ 1 Introducing C# and .NET/Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿Calculator calculator = new();
+
+int answer = calculator.Add(10, 84);
+
+Console.WriteLine($"10 + 84 = {answer}.");
+
+class Calculator
+{
+    public int Add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+}
